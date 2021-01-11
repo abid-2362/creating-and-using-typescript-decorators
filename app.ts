49fb2089e084a53eb54795745b984495b239327c
Abid Ali <abdsoftfsd@gmail.com>
2021-01-11 09:08:33 +0500

    Adds @route and @logRoute decorators

--- app.ts
+++ app.ts
@@ -1,47 +1,44 @@
-function logClass(message: string): ClassDecorator {
-  console.log(`${message} evaluated`);
-  return function (constructor: Function): void {
-    console.log(`${message} called`);
-  }
+import APIServer from './APIServer';
+import { Request, Response } from 'express';
+
+const server = new APIServer();
+
+// ENUMS
+enum methods {
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  DELETE = 'delete',
 }
 
-function logProperty(message: string): PropertyDecorator {
-  console.log(`${message} evaluated`);
-  return function (target: Object, propertyKey: string | symbol): void {
-    console.log(`${message} called`);
-  }
-}
-
-function logMethod(message: string): MethodDecorator {
-  console.log(`${message} evaluated`);
-  return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor): void {
-    console.log(`${message} called`);
-  }
-}
-
-function logParamerer(message: string): ParameterDecorator {
-  console.log(`${message} evaluated`);
-  return function (target: Object, propertyKey: string | symbol, parameterIndex: number): void {
-    console.log(`${message} called`);
-  }
-}
-
-@logClass('Class Decorator')
-class Person {
-  private _directReports: Person[];
+class APIRoutes {
   
-  @logProperty('Property Decorator')
-  public emailAddress: string;
-  
-  constructor(public firstName: string, public lastName: string) {
-    this._directReports = [];
-  }
-  
-  @logMethod('Method Decorator 1')
-  @logMethod('Method Decorator 2')
-  public addDirectReport(@logParamerer('Parameter Decorator') person: Person) {
-    this._directReports.push(person);
+  @logRoute()
+  @route(methods.GET, '/')
+  public indexRoute(req: Request, res: Response) {
+    return {
+      'Hello': 'World',
+    };
   }
 }
 
-const person = new Person('David', 'Tucker');
+function route(method: methods, path: string): MethodDecorator {
+  return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+    server.app[method](path, (req: Request, res: Response) => {
+      res.status(200).json(descriptor.value(req, res));
+    });
+  }
+}
+
+function logRoute(): MethodDecorator {
+  return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+    const original = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      let req = args[0] as Request;
+      console.log(`${req.url} ${req.method} called`);
+      return original.apply(this, args);
+    }
+  }
+}
+
+server.start();
